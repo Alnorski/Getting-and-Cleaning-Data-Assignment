8933447c1a993795deafe58402a1b1bd712476aa
Alnorski <alex_norwood@hotmail.com>
2021-04-11 08:24:01 -0700

    Edit formatting of comments within script

--- run_analysis.R
+++ run_analysis.R
@@ -12,10 +12,10 @@ featuresName <- read.table("./UCI HAR Dataset/features.txt")
 activityLegend <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
 ## 1) "Merges the training and the test sets to create one data set"
-    ## Row-binding specific dataset: Set, Label and Subject
-    ## Then, column-binding all dataset 
-    ## (Note: make "subject", "label" on left side of column)
-    ## Use featuresName for colNames of "set"
+## Row-binding specific dataset: Set, Label and Subject
+## Then, column-binding all dataset 
+## (Note: make "subject", "label" on left side of column)
+## Use featuresName for colNames of "set"
 
 set <- rbind(trainingSet, testSet)
 subject <- rbind(trainingSubject, testSubject)
@@ -23,14 +23,14 @@ label <- rbind(trainingLabel, testLabel)
 allData <- cbind(subject, label, set)
 colnames(allData) <- c("Subject", "ActivityID", featuresName[,2])
 
-## 3) Uses descriptive activity names to name the activities in the data set
-    ## use "ActivityID" as the matching ID for merging allData & activityLegend
-    ## make activityLegend first data in merge function
+## 2) Uses descriptive activity names to name the activities in the data set
+## use "ActivityID" as the matching ID for merging allData & activityLegend
+## make activityLegend first data in merge function
 
 colnames(activityLegend) <- c("ActivityID", "Activity")
 allDataLabeled <- merge(activityLegend, allData, by = "ActivityID", all.y=TRUE)
 
-## 2) Extracts only the mean and standard deviation for each measurement. 
+## 3) Extracts only the mean and standard deviation for each measurement. 
 
 extractedData <- select(allDataLabeled, Subject, Activity, 
                         contains("mean"), contains("std"))
